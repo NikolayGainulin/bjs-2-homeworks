24c8f360cc9180b486e0492252541cc02f3580c7
Nika test <nikolaygtest259@gmail.com>
2025-10-01 18:19:17 +0300

    homework objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,37 @@
+// Функция-конструктор Student
 function Student(name, gender, age) {
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
+  }
   
-}
-
-Student.prototype.setSubject = function (subjectName) {
+  // Метод для установки предмета
+  Student.prototype.setSubject = function (subjectName) {
+    this.subject = subjectName;
+  };
   
-}
-
-Student.prototype.addMarks = function (...marks) {
+  // Метод для добавления оценок
+  Student.prototype.addMarks = function (...marksToAdd) {
+    if (!this.marks) {
+      console.log("Студент отчислен, нельзя добавить оценки");
+      return;
+    }
+    this.marks.push(...marksToAdd);
+  };
   
-}
-
-Student.prototype.getAverage = function () {
+  // Метод для вычисления среднего балла
+  Student.prototype.getAverage = function () {
+    if (!this.marks || this.marks.length === 0) {
+      return 0;
+    }
+    const sum = this.marks.reduce((total, mark) => total + mark, 0);
+    return sum / this.marks.length;
+  };
   
-}
-
-Student.prototype.exclude = function (reason) {
-  
-}
+  // Метод для отчисления студента
+  Student.prototype.exclude = function (reason) {
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
+  };
